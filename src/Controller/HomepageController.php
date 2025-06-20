1c802d47f3ead230e9573f5868b4ddabd656d07a
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-06-20 18:00:45 +0200

    feat(page) Ajout des produits

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findAll();
+
         return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+            'products' => $products,
         ]);
     }
 }
